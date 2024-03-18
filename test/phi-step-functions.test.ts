12d470cffe3505daea88e8ddd231fe5bd75e54ca
= <jaweiss2305@gmail.com>
2024-03-18 10:00:44 -0600

    Initial commit

---
+++ test/phi-step-functions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PhiStepFunctions from '../lib/phi-step-functions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/phi-step-functions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PhiStepFunctions.PhiStepFunctionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
